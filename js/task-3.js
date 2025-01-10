e19cca350ccb7fa04d3aeebf5693a6b7e67df98b
alexdevsprint <alexdevsprint@gmail.com>
2025-01-10 12:55:03 +0200

    commit

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,11 @@
+
+
 console.log('--- Task-3 ---');
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
